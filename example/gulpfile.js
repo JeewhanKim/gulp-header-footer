d70674db19df53a642201aa911d9b953b35bb1e7
JeeWhan Kim <awryong@gmail.com>
2021-06-20 09:30:39 -0400

    support node v12 and gulp v4

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp')
 
-gulp.task('default', () => {
+gulp.task('default', (done) => {
   const headerFooter = require('../gulp-html-header-footer')
   const template = require('./_template.js')
 
   gulp.src(['./*.html'])
   .pipe(headerFooter(gulp.src(['./*.html']), template))
   .pipe(gulp.dest('./dist/'))
+
+  done();
 })
